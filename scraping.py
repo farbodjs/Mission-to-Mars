4fff609d2bba9528309448f5c2807b9a2ddb4ded
Farbod_Jambor <farbodjambor@gmail.com>
2021-10-24 17:50:00 -0700

    adding extera dependencies

--- scraping.py
+++ scraping.py
@@ -4,10 +4,20 @@ import flask_pymongo
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
+    # Run all scraping functions and store results in dictionary
+    data = {
+          "news_title": news_title,
+          "news_paragraph": news_paragraph,
+          "featured_image": featured_image(browser),
+          "facts": mars_facts(),
+          "last_modified": dt.datetime.now()
+    }
+    
 
 def mars_news(browser):
 
